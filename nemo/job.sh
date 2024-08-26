db40cd1c0dcb00b8e8721cfc3e7537e5f3974051
Win Woo <wwoo@google.com>
2024-08-26 13:16:12 +1000

    set global_batch_size to null for 7b

--- nemo/job.sh
+++ nemo/job.sh
@@ -26,38 +26,38 @@ usage() {
 # to view GPU activities on the web terminal
 pip install nvitop
 
-export NCCL_LIB_DIR="/usr/local/nvidia/lib64"
-export NCCL_FASTRAK_IFNAME=eth1,eth2,eth3,eth4,eth5,eth6,eth7,eth8
-export NCCL_FASTRAK_CTRL_DEV=eth0
-export NCCL_SOCKET_IFNAME=eth0
-export NCCL_CROSS_NIC=0
-export NCCL_ALGO=Ring,Tree
-export NCCL_PROTO=Simple
-export NCCL_MIN_NCHANNELS=4
-export NCCL_P2P_NET_CHUNKSIZE=524288
-export NCCL_P2P_PCI_CHUNKSIZE=524288
-export NCCL_P2P_NVL_CHUNKSIZE=1048576
-export NCCL_FASTRAK_NUM_FLOWS=2
-export NCCL_FASTRAK_ENABLE_CONTROL_CHANNEL=0
-export NCCL_BUFFSIZE=8388608
-export NCCL_FASTRAK_USE_SNAP=1
-export NCCL_FASTRAK_USE_LLCM=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export NCCL_NET_GDR_LEVEL=PIX
-export NCCL_FASTRAK_ENABLE_HOTPATH_LOGGING=0
-export NCCL_TUNER_PLUGIN=libnccl-tuner.so
-export NCCL_TUNER_CONFIG_PATH=${NCCL_LIB_DIR}/a3plus_tuner_config.textproto
-export NCCL_SHIMNET_GUEST_CONFIG_CHECKER_CONFIG_FILE=${NCCL_LIB_DIR}/a3plus_guest_config.textproto
-export NCCL_FASTRAK_PLUGIN_ACCEPT_TIMEOUT_MS=600000
-export NCCL_NVLS_ENABLE=0
-export LD_LIBRARY_PATH=${NCCL_LIB_DIR}:${LD_LIBRARY_PATH}
+# export NCCL_LIB_DIR="/usr/local/nvidia/lib64"
+# export NCCL_FASTRAK_IFNAME=eth1,eth2,eth3,eth4,eth5,eth6,eth7,eth8
+# export NCCL_FASTRAK_CTRL_DEV=eth0
+# export NCCL_SOCKET_IFNAME=eth0
+# export NCCL_CROSS_NIC=0
+# export NCCL_ALGO=Ring,Tree
+# export NCCL_PROTO=Simple
+# export NCCL_MIN_NCHANNELS=4
+# export NCCL_P2P_NET_CHUNKSIZE=524288
+# export NCCL_P2P_PCI_CHUNKSIZE=524288
+# export NCCL_P2P_NVL_CHUNKSIZE=1048576
+# export NCCL_FASTRAK_NUM_FLOWS=2
+# export NCCL_FASTRAK_ENABLE_CONTROL_CHANNEL=0
+# export NCCL_BUFFSIZE=8388608
+# export NCCL_FASTRAK_USE_SNAP=1
+# export NCCL_FASTRAK_USE_LLCM=1
+# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+# export NCCL_NET_GDR_LEVEL=PIX
+# export NCCL_FASTRAK_ENABLE_HOTPATH_LOGGING=0
+# export NCCL_TUNER_PLUGIN=libnccl-tuner.so
+# export NCCL_TUNER_CONFIG_PATH=${NCCL_LIB_DIR}/a3plus_tuner_config.textproto
+# export NCCL_SHIMNET_GUEST_CONFIG_CHECKER_CONFIG_FILE=${NCCL_LIB_DIR}/a3plus_guest_config.textproto
+# export NCCL_FASTRAK_PLUGIN_ACCEPT_TIMEOUT_MS=600000
+# export NCCL_NVLS_ENABLE=0
+# export LD_LIBRARY_PATH=${NCCL_LIB_DIR}:${LD_LIBRARY_PATH}
 
-export LD_LIBRARY_PATH="/usr/local/nccl-plugin/lib64:/usr/local/nvidia/lib64/:${LD_LIBRARY_PATH}"
-echo "Warning: Set LD_LIBRARY_PATH=$LD_LIBRARY_PATH to override the NCCL library"
+# export LD_LIBRARY_PATH="/usr/local/nccl-plugin/lib64:/usr/local/nvidia/lib64/:${LD_LIBRARY_PATH}"
+# echo "Warning: Set LD_LIBRARY_PATH=$LD_LIBRARY_PATH to override the NCCL library"
 
-ldconfig /usr/local/nvidia/lib64/
-echo "Added /usr/local/nvidia/lib64/ to ldconfig:"
-ldconfig -p | grep libcuda | sed 's/^/  /'
+# ldconfig /usr/local/nvidia/lib64/
+# echo "Added /usr/local/nvidia/lib64/ to ldconfig:"
+# ldconfig -p | grep libcuda | sed 's/^/  /'
 
 ## To turn on for debugging
 # export TORCH_CPP_LOG_LEVEL=INFO # this is to turn on the verbose torch logs
